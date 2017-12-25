1e74faf0624b091bfd2d5eaeb573716aca7aa27e
Bilal El Houdaigui <bilal.elhoudaigui@gmail.com>
2017-12-25 14:42:54 +0000

    Add files via upload

---
+++ TCDS/plotting.py
@@ -0,0 +1,167 @@
+import TCDS.simulation as sim
+import numpy as np
+import matplotlib.pyplot as plt
+import dnaplotlib as dpl
+# gridspec is a module which specifies the location of the subplot in the figure.
+import matplotlib.gridspec as gridspec
+
+###################### Plotting #######################
+
+def plot_mean_sigma_genes_v2(INI_file, sigma_info, RNAPs_pos_info):
+
+    # path to the input files
+    path = "../example/" # a changer
+    INI_file = path+INI_file
+    # read the config file
+    config = sim.read_config_file(INI_file)
+    # get inputs infos from the config file
+    GFF_file = path+config.get('INPUTS', 'GFF')
+    TSS_file = path+config.get('INPUTS', 'TSS')
+    TTS_file = path+config.get('INPUTS', 'TTS')
+    Prot_file = path+config.get('INPUTS', 'BARR_FIX')
+    DELTA_X = path+config.get('SIMULATION', 'DELTA_X').split("/")[-1]
+    DELTA_X = int(DELTA_X.split("/")[-1])
+    SIGMA_0 = path+config.get('SIMULATION', 'SIGMA_0')
+    SIGMA_0 = float(SIGMA_0.split("/")[-1])
+
+    # load and get BARR_FIX positions
+    prot = sim.load_tab_file(Prot_file)
+    BARR_FIX = (prot['prot_pos'].values).astype(int)
+
+    # To draw the beautiful genes we need to read the GFF, TSS and TTS files to get some info ;)
+    gff_df_raw = sim.load_gff(GFF_file)
+    # to get the cov_bp (a verifier)
+    genome_size = sim.get_genome_size(gff_df_raw)
+    genome = int(genome_size/DELTA_X)
+    cov_bp = np.arange(0, genome_size, DELTA_X)
+    cov_bp = np.resize(cov_bp, genome)
+
+    gff_df = sim.rename_gff_cols(gff_df_raw)
+
+    tss = sim.load_tab_file(TSS_file)
+    Kon = tss['TSS_strength'].values
+
+    tts = sim.load_tab_file(TTS_file)
+    Poff = tts['TTS_proba_off'].values
+
+    strands = sim.str2num(gff_df['strand'].values)
+
+    # Create the figure and all axes to draw to
+    fig = plt.figure(1, figsize=(9,6)) # 3.2,2.7
+    gs = gridspec.GridSpec(2, 1, height_ratios=[9, 3, 1])
+    # Color maps for formatting
+    col_map = {}
+    col_map['red']     = (0.95, 0.30, 0.25)
+    col_map['green']   = (0.38, 0.82, 0.32)
+    col_map['blue']    = (0.38, 0.65, 0.87)
+    col_map['orange']  = (1.00, 0.75, 0.17)
+    col_map['purple']  = (0.55, 0.35, 0.64)
+    col_map['yellow']  = (0.98, 0.97, 0.35)
+    col_map['grey']    = (0.70, 0.70, 0.70)
+    col_map['dark_grey'] = (0.60, 0.60, 0.60)
+    col_map['light_grey'] = (0.9, 0.9, 0.9)
+
+    # CDS formatting options
+    opt_CDSs = []
+
+    Ps = []
+    CDSs = []
+    Ts = []
+
+    design = []
+
+    for i in gff_df.index.values:
+        opt_CDSs.append({'label':'Gene%s \n%.03f'%(str(i+1),Kon[i]), 
+                         'label_style':'italic', 
+                         'label_y_offset':-5, 
+                         'color':col_map['orange']})
+        # Design of the construct
+        if strands[i] == True:
+            # Promoters
+            Ps.append({'type':'Promoter', 'name':'P%s'%str(i+1), 'start':tss['TSS_pos'][i], 
+                       'end':tss['TSS_pos'][i]+5, 'fwd':strands[i], 'opts':{'color':col_map['green']}})
+            # Coding Sequence
+            CDSs.append({'type':'CDS', 'name':'CDS%s'%str(i+1), 'start':gff_df['start'][i],  
+                         'end':gff_df['end'][i], 'fwd':gff_df['strand'][i], 'opts':opt_CDSs[i]}) 
+        else:
+            # Promoters
+            Ps.append({'type':'Promoter', 'name':'P%s'%str(i+1), 'start':tss['TSS_pos'][i], 
+                       'end':tss['TSS_pos'][i]-5, 'fwd':strands[i], 'opts':{'color':col_map['green']}})
+            # Coding Sequence
+            CDSs.append({'type':'CDS', 'name':'CDS%s'%str(i+1), 'start':gff_df['end'][i],  
+                         'end':gff_df['start'][i], 'fwd':gff_df['strand'][i], 'opts':opt_CDSs[i]}) 
+        # Terminators
+        Ts.append({'type':'Terminator', 'name':'T%s'%str(i+1), 'start':tts['TTS_pos'][i], 
+              'end':tts['TTS_pos'][i]+5, 'fwd':strands[i], 'opts':{'color':col_map['red']}})
+
+        # A design is merely a list of parts and their properties
+        if strands[i] == True:
+            design.append(Ps[i])
+            design.append(CDSs[i]) 
+            design.append(Ts[i])
+        else:
+            design.append(Ts[i])
+            design.append(CDSs[i]) 
+            design.append(Ps[i])
+        
+    ax_mean_sig = plt.subplot(gs[0])
+    ax_dna = plt.subplot(gs[1])
+
+    # Redender the DNA
+    dr = dpl.DNARenderer(scale=7, linewidth=1)
+    start, end = dr.renderDNA(ax_dna, design, dr.trace_part_renderers())
+
+    # Set bounds and display options for the DNA axis
+    dna_len = end-start
+    ax_dna.set_xlim([cov_bp[0], cov_bp[-1]]) #start-50
+    ax_dna.set_ylim([-8,8])
+    #ax_dna.plot(5000, 'ro', markersize=15)
+    for xc in BARR_FIX:
+        ax_dna.axvline(x=xc, ymin=0.40, ymax=0.60, color='k', linewidth=5)
+    
+    ax_dna.plot([cov_bp[0],cov_bp[-1]], [0,0], color=(0,0,0), linewidth=1.0, zorder=1)
+    ax_dna.axis('off')
+    
+    # plot of sigma and mean of sigma
+    plt.ion()
+    ax_mean_sig.plot(cov_bp, sigma_info, linewidth= 1.5)
+    ax_mean_sig.legend(loc='best', fontsize = 12)
+    ax_mean_sig.set_ylim([-0.2,0.2])
+    ax_mean_sig.set_xlim([0, cov_bp[-1]])
+
+    #ax_mean_sig.set_title(r"Title goes here", fontsize = 13)
+    ax_mean_sig.set_ylabel(r'Supercoiling density $(\sigma)$')
+    ax_mean_sig.set_xlabel('Position (bp)')
+    ax_mean_sig.plot(RNAPs_pos_info*DELTA_X, np.full(len(RNAPs_pos_info), SIGMA_0, dtype=float), 'ro', markersize=12, label = "RNA Polymerase")
+    ax_mean_sig.set_ylim([-0.2, 0.2])
+    plt.pause(0.001)
+    plt.gcf().clear()
+    plt.show()  
+
+
+# Plot the supercoiling density before and after adding Gyrase (Chong experiment)
+def plot_topoI_gyrase(INI_file, output_dir_pre, output_dir_post):
+
+    GFF_file, TSS_file, TTS_file, Prot_file, m, sigma_t, epsilon, SIGMA_0, DELTA_X, DELTA_T, RNAPS_NB, ITERATIONS_NB, OUTPUT_STEP, GYRASE_CONC, TOPO_CONC, TOPO_CTE, GYRASE_CTE, TOPO_EFFICIENCY, k_GYRASE, x0_GYRASE, k_TOPO, x0_TOPO = sim.read_config_file_v2(INI_file)
+    tss = sim.load_tab_file(TSS_file)
+    Kon = tss['TSS_strength'].values
+
+    # get the full path
+    output_dir_pre = output_dir_pre+"/withSC_Kon_%.06f/RNAPn_%s/Sig0_%s/Gyrase_%s_TopoI_%s/all_res/save_sigma_info.npz" %(Kon[0], RNAPS_NB, SIGMA_0, GYRASE_CONC, TOPO_CONC)
+    output_dir_post = output_dir_post+"/withSC_Kon_%.06f/RNAPn_%s/Sig0_%s/Gyrase_%s_TopoI_%s/all_res/save_sigma_info.npz" %(Kon[0], RNAPS_NB, SIGMA_0, GYRASE_CONC, TOPO_CONC)
+    
+    # get the files
+    sigma_info_pre = np.load(output_dir_pre)
+    sigma_info_post = np.load(output_dir_post)
+    mean_sig_WG_pre = sigma_info_pre["mean_sig_wholeGenome"]
+    mean_sig_WG_post = sigma_info_post["mean_sig_wholeGenome"]
+    mean_sig_WG = np.concatenate([mean_sig_WG_pre, mean_sig_WG_post])
+
+    # plot the result
+    fig = plt.figure(1)
+    plt.plot(range(0, len(mean_sig_WG)*int(DELTA_T), int(DELTA_T)), mean_sig_WG)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Mean of supercoiling density")
+    fig.tight_layout()
+    plt.show()
+
