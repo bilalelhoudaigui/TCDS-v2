a346fed6fbd5090adb180347d89e8d501a0bec6a
Bilal El Houdaigui <bilal.elhoudaigui@gmail.com>
2017-12-26 12:31:20 +0000

    Simplify the output path

--- analysis_scripts/film_from_npz.py
+++ analysis_scripts/film_from_npz.py
@@ -8,11 +8,7 @@ matplotlib.rcParams.update({'font.size': 13})
 INI_file=sys.argv[1]
 output_dir=sys.argv[2]
 
-GFF_file, TSS_file, TTS_file, Prot_file, m, sigma_t, epsilon, SIGMA_0, DELTA_X, DELTA_T, RNAPS_NB, ITERATIONS_NB, OUTPUT_STEP, GYRASE_CONC, TOPO_CONC, TOPO_CTE, GYRASE_CTE, TOPO_EFFICIENCY, k_GYRASE, x0_GYRASE, k_TOPO, x0_TOPO = sim.read_config_file_v2(INI_file)
-tss = sim.load_tab_file(TSS_file)
-Kon = tss['TSS_strength'].values
-
-output_dir_res = output_dir+"/withSC_Kon_%.06f/RNAPn_%s/Sig0_%s/Gyrase_%s_TopoI_%s/all_res" %(Kon[0], RNAPS_NB, SIGMA_0, GYRASE_CONC, TOPO_CONC)
+output_dir_res = output_dir+"/all_res"
 
 sigma_info = np.load(output_dir_res+"/save_sigma_info.npz")
 RNAPs_info = np.load(output_dir_res+"/save_RNAPs_info.npz")
