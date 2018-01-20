e040e602a538d9fe3801655f59130143ac893b95
Bilal El Houdaigui <bilal.elhoudaigui@gmail.com>
2018-01-20 00:16:04 +0000

    add the setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TCDS',
+      version='0.7',
+      description='Python package for the simulation of Transcription-Coupled DNA Supercoiling in Bacteria',
+      url='https://github.com/bilalelhoudaigui/TCDS-v2',
+      author='Bilal El Houdaigui',
+      author_email='bilal.elhoudaigui@gmail.com',
+      license='MIT',
+      packages=['TCDS'],
+      zip_safe=False)
