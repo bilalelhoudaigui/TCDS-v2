4d18a7eaf1775a912bf83b7ecf8f48fdc8dc2dda
sammeyer2017 <sam.meyer@insa-lyon.fr>
2019-06-27 08:28:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='TCDS',
-      version='0.7',
-      description='Python package for the simulation of Transcription-Coupled DNA Supercoiling in Bacteria',
-      url='https://github.com/bilalelhoudaigui/TCDS-v2',
+setup(name='TwisTranscripT',
+      version='1.0',
+      description='Python3 package for the simulation of the Transcription-Supercoiling Coupling in Bacteria',
+      url='https://github.com/sammeyer2017/TwisTranscripT',
       author='Bilal El Houdaigui',
       author_email='bilal.elhoudaigui@gmail.com',
       license='MIT',
-      packages=['TCDS'],
+      packages=['TSC'],
       zip_safe=False)
